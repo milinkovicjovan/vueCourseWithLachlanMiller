ce7ba5577b832dc9d7fc389c17898062318a45a6
milinkovicjovan <milinkovicjovanbusiness@gmail.com>
2022-10-28 18:49:18 +0200

    vueCourse

--- src/main.js
+++ src/main.js
@@ -1,4 +1,60 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import * as Vue from "vue/dist/vue.esm-bundler.js";
 
-createApp(App).mount('#app')
+const Num = {
+  props: ["number"],
+  template: `
+  <button
+     :class="getClass(number)"
+     v-on:click="click"
+  
+  >
+   {{ number }}
+  </button>
+    `,
+  methods: {
+    click() {
+      this.$emit("chosen", this.number);
+    },
+    getClass(number) {
+      return this.isEven(number) ? "blue" : "red";
+    },
+    isEven(number) {
+      return number % 2 === 0;
+    },
+  },
+};
+const app = Vue.createApp({
+  components: {
+    Num,
+  },
+  template: `
+    <num
+      v-for="number in numbers"
+      v-bind:number="number"
+      v-on:chosen="addNumber" /> 
+      
+      <hr />
+      
+    <num
+      v-for="number in numberHistory"
+      v-bind:number="number" />
+    `,
+  data() {
+    return {
+      numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      numberHistory: [],
+    };
+  },
+
+  computed: {
+    evenList() {
+      return this.numbers.filter((num) => this.isEven(num));
+    },
+  },
+  methods: {
+    addNumber(number) {
+      this.numberHistory.push(number);
+    },
+  },
+});
+app.mount("#app");
