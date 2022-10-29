7696b9b19140bac96547f80df47839dc47340c5f
milinkovicjovan <milinkovicjovanbusiness@gmail.com>
2022-10-29 19:28:34 +0200

    compositionApi

---
+++ composition/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>{{ msg }}</div>
+</template>
+
+<script>
+export default {
+  setup() {
+    const msg = "Hello World";
+
+    return {
+      msg,
+    };
+  },
+};
+</script>
+
+<style scoped></style>
