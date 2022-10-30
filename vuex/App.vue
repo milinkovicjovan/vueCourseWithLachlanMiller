531cd677870b985605b8202b82efba5f83b35b65
milinkovicjovan <milinkovicjovanbusiness@gmail.com>
2022-10-30 20:40:23 +0100

    updatingStateWithMutations

---
+++ vuex/App.vue
@@ -0,0 +1,24 @@
+<template>
+  {{ store.state.count }}
+  <button @click="click">Increment</button>
+</template>
+
+<script>
+import { useStore } from "vuex";
+export default {
+  setup() {
+    const store = useStore();
+
+    const click = () => {
+      store.commit("increment", { number: 10 });
+    };
+
+    return {
+      store,
+      click,
+    };
+  },
+};
+</script>
+
+<style></style>
